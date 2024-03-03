060f271d94516deb891782ec29d4f624d0ea3713
AyaobaEstelle <abolarinesther98@gmail.com>
2024-03-03 23:32:15 +0100

    edited css

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,27 @@ import './index.css'
 function App() {
   return (
     <div className="App">
-      <nav>
-      <NavLink className="counter-logo" to="/"><img src="./images/counter-logo.png" alt="logo" />
-         Counter
-      </NavLink>
-      <NavLink className="nav-link" to="/test-error-boundary">Error Boundary</NavLink>
-      <NavLink className="nav-link" to="/about">About</NavLink>
-      </nav>
+      <header className="nav-header">
+        <NavLink className="counter-logo" to="/"><img src="./images/counter-logo.png" alt="logo" />
+          Counter
+        </NavLink>
+        <nav>
+          <NavLink className="nav-link" to="/test-error-boundary">Error Boundary</NavLink>
+          <NavLink className="nav-link" to="/about">About</NavLink>
+        </nav>
+      </header>
       <main>
-      <Routes>
-        <Route path="/" element={<Counter />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/test-error-boundary" element={<TestError />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Counter />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/test-error-boundary" element={<TestError />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </main>
       <footer>
         <p className="text-align-center">
           Copyright &copy; 2024 Ayaoba. <br />
-           All rights reserved.
+          All rights reserved.
         </p>
       </footer>
     </div>
